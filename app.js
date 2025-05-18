0c2cd3886694e7c7a7f5456d59bf7ebcde6620c2
Nuzul Abatony <albtony21@gmail.com>
2025-05-19 06:08:12 +0700

    feat: enable server to start successfully

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.use(express.json());
+app.get('/', (req, res) => res.send('Server on'));
+
+module.exports = app;
