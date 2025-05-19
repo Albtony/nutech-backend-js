c56e31e245e0580d27bb88884644f4f381573527
Nuzul Abatony <albtony21@gmail.com>
2025-05-19 07:13:08 +0700

    chore: configure CORS

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+};
 
 app.use(express.json());
-app.get('/', (req, res) => res.send('Server on'));
+app.use(cors(corsOptions));
+app.get('/', (req, res) => res.status(200).json({ message: 'Server on' }));
 
 module.exports = app;
